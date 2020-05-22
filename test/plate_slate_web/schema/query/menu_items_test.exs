8d63fe798a9cfefe099be99d9b2ca1ea3cff7854
喻林 <yulin_yl@126.com>
2020-05-22 14:03:04 +0800

    support input params

--- test/plate_slate_web/schema/query/menu_items_test.exs
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -21,4 +21,62 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemTest do
              }
            } = json_response(conn, 200)
   end
+
+  @query """
+  {
+    menuItems(matching: "reu") {
+      name
+    }
+  }
+  """
+  test "menuItems field retuns menu items filtered by name", %{conn: conn} do
+    conn = get(conn, "/api", query: @query)
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
+
+  @query """
+  {
+    menuItems(matching: 123) {
+      name
+    }
+  }
+  """
+  test "menuItems field returns errors when using a bad value" do
+    conn = get(conn, "/api", query: @query)
+
+    assert %{
+             "errors" => [
+               %{"message" => message}
+             ]
+           } = json_response(conn, 400)
+
+    assert message =~ ~s|"matching" has invalid value 123|
+  end
+
+  @query """
+  query ($term: String) {
+    menuItems(matching: $term) {
+      name
+    }
+  }
+  """
+  @variables %{"term" => "reu"}
+  test "menuItems field filters by name when using a variable", %{conn: conn} do
+    conn = get(conn, "/api", query: @query, variables: @variables)
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
 end

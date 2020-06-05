67c73151e40d92c843c1882488324e1968b28129
喻林 <yulin_yl@126.com>
2020-06-05 18:15:02 +0800

    user model

--- lib/plate_slate/ordering/order.ex
+++ lib/plate_slate/ordering/order.ex
@@ -1,12 +1,16 @@
 defmodule PlateSlate.Ordering.Order do
   use Ecto.Schema
   import Ecto.Changeset
+
   alias PlateSlate.Ordering.Order
+  alias PlateSlate.Accounts.User
 
   schema "orders" do
     field(:customer_number, :integer, read_after_writes: true)
     field(:ordered_at, :utc_datetime, read_after_writes: true)
     field(:state, :string, read_after_wirtes: true)
+
+    belongs_to(:customer, User)
 
     embeds_many(:items, PlateSlate.Ordering.Item)
 
@@ -16,7 +20,7 @@ defmodule PlateSlate.Ordering.Order do
   @doc false
   def changeset(%Order{} = order, attrs) do
     order
-    |> cast(attrs, [:customer_number, :ordered_at, :state])
+    |> cast(attrs, [:customer_number, :ordered_at, :state, :customer_id])
     |> cast_embed(:items)
   end
 end
